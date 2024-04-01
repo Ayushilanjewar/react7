4c04c4f67520b66bb115b8be1866d7081fb04ab5
prasad-bigdp <bigdp585@gmail.com>
2024-04-01 07:56:29 +0530

    update

---
+++ projects/firebase/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Signup from './Signup'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Authentication</h1>
+      <Signup/>
+    </div>
+  )
+}
+
+export default App
