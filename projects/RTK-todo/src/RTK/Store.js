dff95e6aef37f05ab56f2012adf1cb1c0e60a2eb
prasad-bigdp <bigdp585@gmail.com>
2024-04-05 10:58:19 +0530

    update

---
+++ projects/RTK-todo/src/RTK/Store.js
@@ -0,0 +1,23 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState: [],
+    reducers: {
+        addToDo: (state,action) =>
+        {
+            console.log(action.payload)
+            state.push(action.payload)
+        },
+        deleteToDo: (state, action) =>
+        {
+          return state.filter((todo) => todo.id != action.payload)
+        }
+    }
+})
+export const { addToDo, deleteToDo } = todoSlice.actions
+const store = configureStore({
+    reducer: {
+        todos:todoSlice.reducer
+    }
+})
+export default store;
